d373a7fbcb0dc6630643796d64f744abc69f4343
Tobin Jones <tobin.jones@buckleysystems.com>
2020-12-11 13:14:23 +1300

    Plans return lists

--- wirecode/plans.py
+++ wirecode/plans.py
@@ -8,18 +8,22 @@ def line_plan(start, end, segments, lead_in=None, lead_out=None):
     direction = unit(z2 - z1)
     integration_length = abs(z2 - z1) / segments
 
+    plan = []
+
     # Lead-in for acceleration
     if lead_in is not None:
         lead_in_pt = z1 - direction * lead_in
-        yield GCode(["G01"]).set_position(z_to_p(lead_in_pt))
+        plan += [GCode(["G01"]).set_position(z_to_p(lead_in_pt))]
 
     # Yields (segments + 1) points
     yield GCode(["G01"]).set_position(start)
     for n in range(segments):
         pt = z1 + direction * integration_length * (n + 1)
-        yield GCode(["G01"], comments=["(INT)"]).set_position(z_to_p(pt))
+        plan += [GCode(["G01"], comments=["(INT)"]).set_position(z_to_p(pt))]
 
     # Lead-out for deceleration
     if lead_out is not None:
         lead_out_pt = z2 + direction * lead_out
-        yield GCode(["G01"]).set_position(z_to_p(lead_out_pt))
+        plan += [GCode(["G01"]).set_position(z_to_p(lead_out_pt))]
+
+    return plan
