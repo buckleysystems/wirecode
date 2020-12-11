e7ab9708ca467b5b52f32f73aff346ac8a948594
Tobin Jones <tobin.jones@buckleysystems.com>
2020-12-11 13:19:48 +1300

    Square plan

--- wirecode/plans.py
+++ wirecode/plans.py
@@ -1,5 +1,6 @@
 from .geometry import p_to_z, z_to_p, unit
 from .gcode import GCode
+from math import e, pi, sqrt
 
 
 def line_plan(start, end, segments, lead_in=None, lead_out=None):
@@ -27,3 +28,21 @@ def line_plan(start, end, segments, lead_in=None, lead_out=None):
         plan += [GCode(["G01"]).set_position(z_to_p(lead_out_pt))]
 
     return plan
+
+
+def _square_edge(quadrant, radius, segments, accel_length):
+    """One edge of a square plan"""
+    ext_radius = 2 * radius / sqrt(2)
+    side_length = 2 * radius
+    rotation = e ** (1j * pi / 2 * (quadrant - 1))
+    unit_vec = unit(-1 + 1j) * rotation
+    start_pt = z_to_p((ext_radius + 0j) * rotation)
+    end_pt = z_to_p(start_pt + side_length * unit_vec)
+    return line_plan(start_pt, end_pt, segments, accel_length, accel_length)
+
+
+def square_plan(radius, segments_per_side, accel_length):
+    sides = [
+        _square_edge(q, radius, segments_per_side, accel_length) for q in (1, 2, 3, 4)
+    ]
+    return sum(sides, start=[])
